84790ec558d32161ffb673f88304abc3def7c661
BlueKrow <ndioses@gmail.com>
2022-02-21 10:52:08 +0100

    test: add host awake testing

---
+++ Nke.NetTools.WakeOnLan.Tests/HostShould.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using FluentAssertions;
+using Xunit;
+
+namespace Nke.NetTools.WakeOnLan.Tests
+{
+    public class HostShould
+    {
+        [Fact]
+        public void DetectIfAHostIsNotAwake()
+        {
+            var testIpAddress = IPAddress.Parse("192.168.5.5");
+            var destinationHost = new Host(testIpAddress);
+            destinationHost.IsAwake().Should().BeFalse();
+        }
+        
+        [Fact]
+        public void DetectIfAHostIsAwake()
+        {
+            var testIpAddress = IPAddress.Parse("127.0.0.1");
+            var destinationHost = new Host(testIpAddress);
+            destinationHost.IsAwake().Should().BeTrue();
+        }
+
+    }
+}
